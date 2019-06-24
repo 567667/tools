1e1668e51f9d4b7f2076acb893bb835d26a6d761
andreykot <kotovskyandrew@yandex.ru>
2019-06-25 02:54:43 +0300

    make class GridBuilder, works with intersection

--- shp_mesh_builder/main.py
+++ shp_mesh_builder/main.py
@@ -1,133 +1,136 @@
 import os
-from osgeo import ogr, gdal
-
-gdal.PushErrorHandler('CPLQuietErrorHandler')
+from osgeo import ogr, osr
 
 
-def grid_points(x1, y1, x2, y2, step_x, step_y):
-    '''
-    Calculate coordinates of points for GRID
-    :param x1:
-    :param y1:
-    :param x2:
-    :param y2:
-    :param step_x:
-    :param step_y:
-    :return: tuple
+class GridBuilder:
+    def __init__(self,
+                 prj: osr.SpatialReference,
+                 extent: tuple,
+                 step_x=1,
+                 step_y=1,
+                 driver=ogr.GetDriverByName("ESRI Shapefile")):
+        self.prj = prj
+        self.extent = extent
+        self.step_x = step_x
+        self.step_y = step_y
+        self.driver = driver
 
-    x1y1-----x2y2
-    |          |
-    |          |
-    |          |
-    x4y4-----x3y3
+    def grid_points(self):
+        """
+        Calculate coordinates of polygons for GRID.
+        x1y1-----x2y2
+        |          |
+        |          |
+        |          |
+        x4y4-----x3y3
+        """
+        x1, y1 = self.extent[0], self.extent[3]
+        x2, y2 = self.extent[1], self.extent[2]
 
-    '''
+        x_start = (x1 // self.step_x) * self.step_x
+        y_start = (y1 // self.step_y) * self.step_y + self.step_y
+        x_end = (x2 // self.step_x) * self.step_x + self.step_x
+        y_end = (y2 // self.step_y) * self.step_y + self.step_y
 
-    x_start = (x1 // step_x) * step_x
-    y_start = (y1 // step_y) * step_y
-    x_end = (x2 // step_x) * step_x + step_x
-    y_end = (y2 // step_y) * step_y + step_y
+        print('grid boundary: ', x_start, y_start, x_end, y_end)
 
-    print('grid boundary: ', x_start, y_start, x_end, y_end)
+        _x, _y = x_start, y_start
 
-    _x, _y = x_start, y_start
+        while _x <= x_end and _y >= y_end:
+            x1, y1 = _x, _y
+            x2, y2 = _x + self.step_x, _y
+            x3, y3 = _x + self.step_x, _y - self.step_y
+            x4, y4 = _x, _y - self.step_y
 
-    while _x <= x_end and _y >= y_end:
-        x1, y1 = _x, _y
-        x2, y2 = _x + step_x, _y
-        x3, y3 = _x + step_x, _y - step_y
-        x4, y4 = _x, _y - step_y
+            if _x + self.step_x > x_end:
+                _x, _y = x_start, _y - self.step_y
+            else:
+                _x = _x + self.step_x
+                yield x1, y1, x2, y2, x3, y3, x4, y4
 
-        if _x + step_x == x_end:
-            _x, _y = x_start, _y - step_y
+    @staticmethod
+    def polygon(x1, y1, x2, y2, x3, y3, x4, y4):
+        """
+        Create polygon geometry by coordinates.
+        """
+        ring = ogr.Geometry(ogr.wkbLinearRing)
+        ring.AddPoint(x1, y1)
+        ring.AddPoint(x2, y2)
+        ring.AddPoint(x3, y3)
+        ring.AddPoint(x4, y4)
+
+        # Create polygon #1
+        poly = ogr.Geometry(ogr.wkbPolygon)
+        poly.AddGeometry(ring)
+        return poly
+
+    @staticmethod
+    def create_empty_shp(self, path, prj, geometry=ogr.wkbPolygon):
+        if os.path.exists(os.path.dirname(path)):
+            datasource = self.driver.CreateDataSource(path)
+            layer = datasource.CreateLayer('grid_layer', prj, geometry, options=['ENCODING=UTF-8'])
         else:
-            _x = _x + step_x
-            yield x1, y1, x2, y2, x3, y3, x4, y4
+            raise ValueError("Path doesn't exist")
 
-
-def create_shp(path, name, prj, writeobj=None, geometry=ogr.wkbPolygon):
-    driver = ogr.GetDriverByName("ESRI Shapefile")
-    if os.path.exists(path):
-        datasource = driver.CreateDataSource(os.path.join(path, name))
-        layer = datasource.CreateLayer(name, prj, geometry, options=['ENCODING=UTF-8'])
-    else:
-        print("Path doesn't exist")
-        return
-
-    if writeobj is not None:
         # Add the fields we're interested in
         field_name = ogr.FieldDefn("Name", ogr.OFTString)
         field_name.SetWidth(24)
         layer.CreateField(field_name)
 
-        # create the feature
-        feature = ogr.Feature(layer.GetLayerDefn())
-        feature.SetGeometry(writeobj)
-        layer.CreateFeature(feature)
-        del feature
+        del layer
         del datasource
-        return
+
+    def create_grid(self, path):
+        self.create_empty_shp(path, self.prj)
+        source = self.driver.Open(path, 1)
+        layer = source.GetLayer()
+
+        for grid_poly in self.grid_points():
+            featureDefn = layer.GetLayerDefn()
+            feature = ogr.Feature(featureDefn)
+            feature.SetGeometry(self.polygon(*grid_poly))
+            layer.CreateFeature(feature)
+            del feature
+
+        del layer
+        del source
+
+    def intersection(self, grid_path, target_path):
+        grid_source = self.driver.Open(grid_path, 1)
+        grid_layer = grid_source.GetLayer()
+
+        target_source = self.driver.Open(target_path, 1)
+        target_layer = target_source.GetLayer()
+
+        for feature1 in grid_layer:
+            geom1 = feature1.GetGeometryRef()
+            attribute1 = feature1.GetField('Name')
+            for feature2 in target_layer:
+                geom2 = feature2.GetGeometryRef()
+                # select only the intersections
+                if geom2.Intersects(geom1):
+                    intersection = geom2.Intersection(geom1)
+                    dstfeature = ogr.Feature(dstlayer.GetLayerDefn())
+                    dstfeature.SetGeometry(intersection)
+                    dstfeature.setField(attribute1)
+                    dstfeature.setField(attribute2)
+                    dstfeature.Destroy()
 
 
-def import_shp(shapefile):
-    driver = ogr.GetDriverByName("ESRI Shapefile")
-
-    shpsource = driver.Open(shapefile, 1)
-    layer = shpsource.GetLayer()
-    extent = layer.GetExtent()
-    prj = layer.GetSpatialRef()
-
-    return [layer, extent, prj]
-
-
-def polygon(x1, y1, x2, y2, x3, y3, x4, y4):
-    # Create ring
-    ring = ogr.Geometry(ogr.wkbLinearRing)
-    ring.AddPoint(x1, y1)
-    ring.AddPoint(x2, y2)
-    ring.AddPoint(x3, y3)
-    ring.AddPoint(x4, y4)
-
-    # Create polygon #1
-    poly = ogr.Geometry(ogr.wkbPolygon)
-    poly.AddGeometry(ring)
-    return poly
-
-
-def main():
-    shp = r"C:\Users\kotov\Documents\github_kot\swd\shp_mesh_builder\data\poly_wgs84.shp"
-    newshp = r"C:\Users\kotov\Documents\github_kot\swd\shp_mesh_builder\data\testing"
-
-    shp = import_shp(shp)
-    print(shp)
-
-    create_shp(
-        r"C:\Users\kotov\Documents\github_kot\swd\shp_mesh_builder\data\testing",
-        'create1.shp',
-        shp[2],
-        writeobj=True
-    )
 
 
 def test_main():
-    shp = r"C:\Users\kotov\Documents\github_kot\swd\shp_mesh_builder\data\poly_wgs84.shp"
     driver = ogr.GetDriverByName("ESRI Shapefile")
-    shp = driver.Open(shp)
-    layer = shp.GetLayer()
+    source = driver.Open(r"C:\Users\kotov\Documents\github_kot\swd\shp_mesh_builder\data\poly_wgs84.shp")
+    layer = source.GetLayer()
     prj = layer.GetSpatialRef()
     extent = layer.GetExtent()
-    multipolygon = ogr.Geometry(ogr.wkbMultiPolygon)
-    for grid_poly in grid_points(extent[0], extent[3], extent[1], extent[2], step_x=10, step_y=10):
-        multipolygon.AddGeometry(polygon(*grid_poly))
 
-    create_shp(r"C:\Users\kotov\Documents\github_kot\swd\shp_mesh_builder\data\testing",
-               'newshp.shp',
-               prj,
-               writeobj=multipolygon,
-               geometry=ogr.wkbPolygon)
-    del shp
-    del multipolygon
+    grid = GridBuilder(prj=prj, extent=extent, step_x=10, step_y=10)
+
+    path = r"C:\Users\kotov\Documents\github_kot\swd\shp_mesh_builder\data\testing\setka.shp"
+    grid.create_grid(path)
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
     test_main()
